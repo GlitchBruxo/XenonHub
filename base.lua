7e845c10e504cd4169883ca6cfb753b1152bedb4
DK | GlitchBruxo <glitchbruxo@gmail.com>
2025-03-26 22:11:30 -0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -1,1 +1,43 @@
+-- // Xenon Hub
+-- Load Rayfield
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+-- Game Detection
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    print("X Hub: Blox Fruits detectado! Carregando script...")
+    -- Game script
+    local Window = Rayfield:CreateWindow({
+   Name = "Xenon Hub | Teste",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Xenon Hub In Testing",
+   LoadingSubtitle = "By xGlitchBruxo",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = xenon, -- Create a custom folder for your hub/game
+      FileName = "hubconfig"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "u9XmasadwK", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Key System Beta",
+      Subtitle = "Key System For Beta Protections",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"xenon"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+else
+    print("X Hub: Este jogo não é compatível.")
+end
