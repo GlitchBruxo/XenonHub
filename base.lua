f4176a174e6bd4ec812a62727fc6100a437582b4
DK | GlitchBruxo <glitchbruxo@gmail.com>
2025-03-26 22:40:43 -0300

    Update base.lua

--- base.lua
+++ base.lua
@@ -1,18 +1,98 @@
 -- // Xenon Hub
--- Load Orion
+local KeyGuardLibrary = loadstring(game:HttpGet("https://cdn.keyguardian.org/library/v1.0.0.lua"))()
+local trueData = "d6ae154329b949af9dd5f2d2c4b0d3f2"
+local falseData = "cc7d9d17242d4b769bb17b1d3adcde6c"
+
+KeyGuardLibrary.Set({
+	publicToken = "dd2f16d2b85a4a76ad75fc2c9a847822",
+	privateToken = "7d44a199b72f4e60971f2aab7e9a456f",
+	trueData = trueData,
+	falseData = falseData,
+})
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local key = ""
+
+local Window = Fluent:CreateWindow({
+		Title = "Key System",
+		SubTitle = "Xenon Hub ",
+		TabWidth = 160,
+		Size = UDim2.fromOffset(580, 340),
+		Acrylic = false,
+		Theme = "Dark",
+		MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+		KeySys = Window:AddTab({ 
+		  Title = "Key System", 
+		  Icon = "key" }),
+}
+
+local Entkey = Tabs.KeySys:AddInput("Input", {
+		Title = "Enter Key",
+		Description = "Enter Key Here",
+		Default = "",
+		Placeholder = "Enter key…",
+		Numeric = false,
+		Finished = false,
+		Callback = function(Value)
+				key = Value
+		end
+})
+
+local Checkkey = Tabs.KeySys:AddButton({
+		Title = "Check Key",
+		Description = "Enter Key before pressing this button",
+		Callback = function()
+				local response = KeyGuardLibrary.validateDefaultKey(key)
+				if response == trueData then
+						print("Key is valid"
+						  --
+						-- Load Orion
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 -- Game Detection
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     print("X Hub: Blox Fruits detectado! Carregando script...")
     -- Game script
-    local Window = OrionLib:MakeWindow({
-      Name = "Xenon Hub | Beta", 
-      HidePremium = false, 
-      SaveConfig = true, 
-      ConfigFolder = "xenon",
-      IntroEnabled = true,
-      IntroText = "Xenon Hub"
-    })
+    local Window = Rayfield:CreateWindow({
+   Name = "Xenon Hub | Teste",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Xenon Hub In Testing",
+   LoadingSubtitle = "By xGlitchBruxo",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = xenon, -- Create a custom folder for your hub/game
+      FileName = "hubconfig"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "u9XmasadwK", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   }
+})
 else
     print("X Hub: Este jogo não é compatível.")
 end
+--
+				else
+						print("Key is invalid")
+				end
+		end
+})
+
+local Getkey = Tabs.KeySys:AddButton({
+		Title = "Get Key",
+		Description = "Get Key here",
+		Callback = function()
+				setclipboard(KeyGuardLibrary.getLink())
+		end
+})
+
+Window:SelectTab(1)
