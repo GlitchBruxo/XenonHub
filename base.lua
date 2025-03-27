c3f489606824406c1fce71c8d3853e40e5dcdb25
DK | GlitchBruxo <glitchbruxo@gmail.com>
2025-03-26 21:03:48 -0300

    Create base.lua

---
+++ base.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    print("Xenon Hub: Blox Fruits detectado! Executando script...")
+    -- Aqui você coloca o código do script
+else
+    print("Xenon Hub: Este jogo não é compatível.")
+end
